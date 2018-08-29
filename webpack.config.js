46f24d8b9ce191cd4046e199aeb62fa88619741f
azrsix <azrsix@gmail.com>
2018-08-29 22:12:17 +0900

    azrsix.github.io

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ const config = {
       use: [{
         loader: 'babel-loader',
         options: {
-          presets: ['es2015']
+          presets: ['@babel/preset-env']
         }
       }]
     }, {
