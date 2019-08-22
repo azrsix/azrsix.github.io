bdf297b0e56c87db246c34d8e0130964700d8545
azrsix <azrsix@gmail.com>
2019-08-22 23:51:16 +0900

    azrsix.github.io

--- src/js/app.js
+++ src/js/app.js
@@ -3,3 +3,8 @@
 import ue from 'ue-scroll-js';
 
 ue.init();
+
+const yearBox = document.querySelector('.copyright > .year');
+const sinceYear = Number(yearBox.innerHTML);
+const thisYear  = new Date().getFullYear();
+if (sinceYear < thisYear) yearBox.insertAdjacentHTML('beforeend', `-${thisYear}`);
