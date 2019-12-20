ff4e57c125d6074d57418f65cb675b51b336035a
azrsix <azrsix@gmail.com>
2019-12-20 22:49:54 +0900

    azrsix.github.io

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,17 @@
-'use strict';
+'use strict'
 
-import ue from 'ue-scroll-js';
+import { dom, library } from '@fortawesome/fontawesome-svg-core'
+import { faGithub, faTwitter } from '@fortawesome/free-brands-svg-icons'
+import { faHome, faKeyboard, faPen } from '@fortawesome/free-solid-svg-icons'
 
-ue.init();
+import ue from 'ue-scroll-js'
 
-const yearBox = document.querySelector('.js-copyright > .js-year');
-const sinceYear = Number(yearBox.innerHTML);
-const thisYear  = new Date().getFullYear();
-if (sinceYear < thisYear) yearBox.insertAdjacentHTML('beforeend', `-${thisYear}`);
+library.add(faGithub, faTwitter, faHome, faKeyboard, faPen)
+dom.i2svg();
+
+ue.init()
+
+const yearBox = document.querySelector('.js-copyright > .js-year')
+const sinceYear = Number(yearBox.innerHTML)
+const thisYear  = new Date().getFullYear()
+if (sinceYear < thisYear) yearBox.insertAdjacentHTML('beforeend', `-${thisYear}`)
